cc1d644b8dbc6d82a289fd1a2c837d2d10d6c05f
Russell Damerell-Moss <russell.damerell@me.com>
2024-11-27 10:19:01 +0200

    prepare project structure

---
+++ day-01/src/main.rs
@@ -0,0 +1,13 @@
+#[allow(unused_variables)]
+
+fn main() {
+    let exampledata_1 = "";
+    let data_1 = include_str!("../data/data-1.in");
+    let exampledata_2 = "";
+    let data_2 = include_str!("../data/data-2.in");
+    part_one(exampledata_1);
+    part_two(exampledata_2);
+}
+
+fn part_one(data: &str) {}
+fn part_two(data: &str) {}
